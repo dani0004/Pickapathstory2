378424857394f76f964dfce944d3e47964d3b1fb
dani0004 <dani0004@algonquinlive.com>
2014-03-15 05:49:03 -0400

    Initial Commit

---
+++ PickAPathStory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PickAPathStory
+//
+//  Created by Wendy-Anne Daniel on 2014-03-15.
+//  Copyright (c) 2014 Wendy-Anne Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PickAPathStory/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PickAPathStory
+//
+//  Created by Wendy-Anne Daniel on 2014-03-15.
+//  Copyright (c) 2014 Wendy-Anne Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
